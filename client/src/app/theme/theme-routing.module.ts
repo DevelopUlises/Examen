7adf237b00205f5f1b37b86a87ee2e6224eb8c44
Ulises Barbaro Armenteros Torres <armenterosu@gmail.com>
2018-04-07 20:50:26 +0200

    Uploading!!!

---
+++ client/src/app/theme/theme-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { ThemeComponent } from './theme.component';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from "../auth/_guards/auth.guard";
+
+const routes: Routes = [
+    {
+        "path": "",
+        "component": ThemeComponent,
+        "canActivate": [AuthGuard],
+        "children": [
+            {
+                "path": "users",
+                "loadChildren": ".\/pages\/default\/users\/users.module#UsersModule"
+            },
+            {
+                "path": "",
+                "redirectTo": "users",
+                "pathMatch": "full"
+            }
+        ]
+    },
+    {
+        "path": "**",
+        "redirectTo": "404",
+        "pathMatch": "full"
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class ThemeRoutingModule { }
